8ee71d65e7a6cb722d1321750caedf155bca8c17
C0RT1CERA <UJALJOHNSONS@GMAIL.COM>
2023-02-23 09:42:51 +0000

    fgg

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import tailwind from "@astrojs/tailwind";
 
 //netlify wala https://www.netlify.com/blog/astro-ssr/
 //import netlify from '@astrojs/netlify/functions'; saind astro.js website
-import netlify from '@astrojs/netlify/edge-functions';
+//import netlify from '@astrojs/netlify/edge-functions';
 
-// https://astro.build/config
+// https://astro.build/config output: 'server',
+// adapter: netlify(),
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  
   integrations: [tailwind()]
 });
 
